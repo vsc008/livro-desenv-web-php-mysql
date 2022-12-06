9888a8c6a42c490a640a3f7ea4d8a9baeaaa416f
Vinícius Cucolo <vsc008@gmail.com>
2022-12-05 23:03:25 -0300

    item 9.7 - validando edição de tarefas

--- tarefas/editar.php
+++ tarefas/editar.php
@@ -5,13 +5,20 @@ include "banco.php";
 include "ajudantes.php";
 
 $exibir_tabela = false;
+$tem_erros = false;
+$erros_validacao = array();
 
-if (isset($_POST['nome']) && $_POST['nome'] != '') {
+if (tem_post()) {
   $tarefa = array();
 
   $tarefa['id'] = $_POST['id'];
 
-  $tarefa['nome'] = $_POST['nome'];
+  if (isset($_POST['nome']) && strlen($_POST['nome']) > 0) {
+    $tarefa['nome'] = $_POST['nome'];
+  } else {
+    $tem_erros = true;
+    $erros_validacao['nome'] = 'O nome da tarefa é obrigatório!';
+  }
 
   if (isset($_POST['descricao'])) {
     $tarefa['descricao'] = $_POST['descricao'];
@@ -19,8 +26,13 @@ if (isset($_POST['nome']) && $_POST['nome'] != '') {
     $tarefa['descricao'] = '';
   }
 
-  if (isset($_POST['prazo'])) {
-    $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);
+  if (isset($_POST['prazo']) && strlen($_POST['prazo']) > 0) {
+    if (validar_data($_POST['prazo'])) {
+      $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);
+    } else {
+      $tem_erros = true;
+      $erros_validacao['prazo'] = 'O prazo não é uma data válida!';
+    }
   } else {
     $tarefa['prazo'] = '';
   }
@@ -33,11 +45,19 @@ if (isset($_POST['nome']) && $_POST['nome'] != '') {
     $tarefa['concluida'] = 0;
   }
 
-  editar_tarefa($conexao, $tarefa);
-  header('Location: tarefas.php');
-  die();
+  if (! $tem_erros) {
+    editar_tarefa($conexao, $tarefa);
+    header('Location: tarefas.php');
+    die();
+  }
 }
 
-$tarefa = buscar_tarefa($conexao, $_POST['id']);
+$tarefa = buscar_tarefa($conexao, $_GET['id']);
+
+$tarefa['nome'] = (isset($_POST['nome'])) ? $_POST['nome'] : $tarefa['nome'];
+$tarefa['descricao'] = (isset($_POST['descricao'])) ? $_POST['descricao'] : $tarefa['descricao'];
+$tarefa['prazo'] = (isset($_POST['prazo'])) ? traduz_data_para_banco($_POST['prazo']) : $tarefa['prazo'];
+$tarefa['prioridade'] = (isset($_POST['prioridade'])) ? $_POST['prioridade'] : $tarefa['prioridade'];
+$tarefa['concluida'] = (isset($_POST['concluida'])) ? $_POST['concluida'] : $tarefa['concluida'];
 
 include "template.php";
